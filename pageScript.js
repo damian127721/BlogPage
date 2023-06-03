88d7d676b113304ab02e291f98d280eaff2f5272
damian127721 <106865168+damian127721@users.noreply.github.com>
2023-06-03 14:36:39 +0200

    Update Blog postu
    Problem s prepisujicima de DOM elementama

--- pageScript.js
+++ pageScript.js
@@ -1,6 +1,8 @@
 const signButton = document.querySelector(".header__signup__button")
 const logForm = document.getElementById("register-form")
 const logFormCross = document.querySelector(".register-form__cross") 
+let addPostText = document.getElementById("blog-posts__add__textarea")
+const blogPosts = document.getElementById("blog-posts")
 
 signButton.addEventListener("click", function() {
     logForm.style.display = "block"
@@ -11,3 +13,15 @@ logFormCross.addEventListener("click", function() {
     logForm.style.display = "none"
     
 })
+
+function addPostBtn() {
+    addPostText = document.getElementById("blog-posts__add__textarea")
+    let text = addPostText.value
+    newPost(text)
+}
+
+function newPost(text) {
+    blogPosts.innerHTML += `<article class="blog-posts__post">
+    <p class="blog-posts__post__p"> ${text} </p>
+</article> `
+}
