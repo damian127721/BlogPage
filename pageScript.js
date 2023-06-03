21e9c4a1cf11926ec1f2a996eb548db1eb6f5436
damian127721 <106865168+damian127721@users.noreply.github.com>
2023-06-03 18:11:20 +0200

    Firebase base settings
    Podivat se na video v zalozkach jak funguje firebase s nbm

--- pageScript.js
+++ pageScript.js
@@ -1,14 +1,22 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js"
+import { getDatabase, ref, push, remove } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js"
+
 const signButton = document.querySelector(".header__signup__button")
 const logForm = document.getElementById("register-form")
 const logFormCross = document.querySelector(".register-form__cross") 
 const addPostText = document.getElementById("add-post__textarea")
 const blogPosts = document.getElementById("blog-posts")
+const addPostBtn = document.getElementById("add-post__plus")
 
-appSettings = {
+const appSettings = {
     databaseURL: "https://blogpage-37761-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
-
+const app = initializeApp(appSettings)
+console.log(app)
+const database = getDatabase(app)
+const postsData = ref(database, "postsData")
+push(postsData, "test")
 
 signButton.addEventListener("click", function() {
     logForm.style.display = "block"
@@ -20,12 +28,12 @@ logFormCross.addEventListener("click", function() {
     
 })
 
-function addPostBtn() {
+addPostBtn.addEventListener("click", function() {
     let text = addPostText.value
     addPostText.value = ""
     newPost(text)
     
-}
+})
 
 function newPost(text) {
     blogPosts.innerHTML += `<article class="blog-posts__post">
