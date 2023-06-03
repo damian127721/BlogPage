19d2abeef4cad0de131e90309876d8616411ad97
damian127721 <106865168+damian127721@users.noreply.github.com>
2023-06-03 16:18:53 +0200

    Installing node.js

--- pageScript.js
+++ pageScript.js
@@ -1,8 +1,14 @@
 const signButton = document.querySelector(".header__signup__button")
 const logForm = document.getElementById("register-form")
 const logFormCross = document.querySelector(".register-form__cross") 
-let addPostText = document.getElementById("blog-posts__add__textarea")
+const addPostText = document.getElementById("add-post__textarea")
 const blogPosts = document.getElementById("blog-posts")
+
+appSettings = {
+    databaseURL: "https://blogpage-37761-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+
 
 signButton.addEventListener("click", function() {
     logForm.style.display = "block"
@@ -15,13 +21,16 @@ logFormCross.addEventListener("click", function() {
 })
 
 function addPostBtn() {
-    addPostText = document.getElementById("blog-posts__add__textarea")
     let text = addPostText.value
+    addPostText.value = ""
     newPost(text)
+    
 }
 
 function newPost(text) {
     blogPosts.innerHTML += `<article class="blog-posts__post">
+    <p class="blog-posts__post__p">Author:  </p> 
     <p class="blog-posts__post__p"> ${text} </p>
 </article> `
+    
 }
